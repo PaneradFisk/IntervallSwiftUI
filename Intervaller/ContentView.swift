f429f442857eb9e54350638ded6bc8e3f4908b3f
André Bergvall <andre.bergvall@icloud.com>
2020-05-11 20:24:57 +0200

    Initial Commit

---
+++ Intervaller/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Intervaller
+//
+//  Created by André Bergvall on 2020-05-11.
+//  Copyright © 2020 André Bergvall. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
